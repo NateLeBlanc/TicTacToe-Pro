2a15472668029dd9c81165d7e705e510f173c421
Nathan <nathan.leblanc171@gmail.com>
2024-12-19 16:14:34 -0500

    [P1] Create GameDatabase

--- TicTacToe/ConsoleServer/Program.cs
+++ TicTacToe/ConsoleServer/Program.cs
@@ -9,12 +9,3 @@ public class Program
         game.Start();
     }
 }
-
-// TODO: Add a score board with a database to keep track of the players' names, win, and loses.
-// TODO: Add a bot / AI
-// TODO: Add difficulty to the bot
-// TODO: Make an adaptable board size
-// TODO: Make a random board size mode where the objective is still to get 3 in a row but some cells are blocked
-// TODO: Make a hidden board so that the display isn't shown until the end of the game. invalid moves aren't recorded
-// TODO: Give recorded players elo rankings
-// TODO: If AI is implemented, make it so that the AI learns from its loses and other players games and strategies
---
+++ TicTacToe/GameDatabase/PlayerRepository.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+
+namespace GameDatabase
+{
+    public class PlayerRepository
+    {
+        private readonly string _connectionString;
+
+        public PlayerRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void SaveScore(char player, bool isWin)
+        {
+            using var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+
+            using var command = new NpgsqlCommand("INSERT INTO Scores (Player, IsWin, Date) VALUES (@player, @isWin, @date)", connection);
+            command.Parameters.AddWithValue("player", player);
+            command.Parameters.AddWithValue("isWin", isWin);
+            command.Parameters.AddWithValue("date", DateTime.UtcNow);
+
+            command.ExecuteNonQuery();
+        }
+
+        public int GetPlayerScore(char player)
+        {
+            using var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+
+            using var command = new NpgsqlCommand("SELECT COUNT(*) FROM Scores WHERE Player = @player AND IsWin = true", connection);
+            command.Parameters.AddWithValue("player", player);
+
+            return Convert.ToInt32(command.ExecuteScalar());
+        }
+    }
+}
