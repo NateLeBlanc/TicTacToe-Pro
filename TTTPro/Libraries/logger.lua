574ca901902a4f8728a0baa4cecd9defec3f7358
Nathan <nathan.leblanc171@gmail.com>
2025-06-27 17:42:41 -0400

    And file writting to Logger

--- TTTPro/Libraries/logger.lua
+++ TTTPro/Libraries/logger.lua
@@ -1,7 +1,20 @@
 local Logger = {}
 
+local logFilePath = "log.txt"
+
+local function writeToFile(message)
+    local file = io.open(logFilePath, "a")
+    if file then
+        file:write(message .. "\n")
+        file:close()
+    else
+        print("ERROR: Unable to open log file.")
+    end
+end
+
 function Logger.log(level, message)
     print(string.format("[%s] %s", level, message))
+    writeToFile(message)
 end
 
 function Logger.debug(message)
